9e4aed699d700480ad83207e0f88930a03aa2c02
Pawel Moll <Pawel.Moll@arm.com>
2013-01-29 16:30:22 +0000

    clk: vexpress: Add separate SP810 driver
    Split SP810 clocking code into separate driver, selecting
    better (faster) parent at clk_prepare() time. This is to
    avoid problems with clock infrastructure initialization
    order, in particular to avoid dependency of fixed clock
    being initialized before SP810.
    
    Signed-off-by: Pawel Moll <pawel.moll@arm.com>
    Signed-off-by: Catalin Marinas <catalin.marinas@arm.com>

--- drivers/clk/versatile/clk-vexpress.c
+++ drivers/clk/versatile/clk-vexpress.c
@@ -15,40 +15,11 @@
 #include <linux/clk-provider.h>
 #include <linux/err.h>
 #include <linux/of.h>
-#include <linux/of_address.h>
 #include <linux/vexpress.h>
 
 #include <asm/hardware/sp810.h>
 
-static struct clk *vexpress_sp810_timerclken[4];
 static DEFINE_SPINLOCK(vexpress_sp810_lock);
-
-static void __init vexpress_sp810_init(void __iomem *base)
-{
-	int i;
-
-	if (WARN_ON(!base))
-		return;
-
-	for (i = 0; i < ARRAY_SIZE(vexpress_sp810_timerclken); i++) {
-		char name[12];
-		const char *parents[] = {
-			"v2m:refclk32khz", /* REFCLK */
-			"v2m:refclk1mhz" /* TIMCLK */
-		};
-
-		snprintf(name, ARRAY_SIZE(name), "timerclken%d", i);
-
-		vexpress_sp810_timerclken[i] = clk_register_mux(NULL, name,
-				parents, 2, 0, base + SCCTRL,
-				SCCTRL_TIMERENnSEL_SHIFT(i), 1,
-				0, &vexpress_sp810_lock);
-
-		if (WARN_ON(IS_ERR(vexpress_sp810_timerclken[i])))
-			break;
-	}
-}
-
 
 static const char * const vexpress_clk_24mhz_periphs[] __initconst = {
 	"mb:uart0", "mb:uart1", "mb:uart2", "mb:uart3",
@@ -58,6 +29,11 @@ static const char * const vexpress_clk_24mhz_periphs[] __initconst = {
 void __init vexpress_clk_init(void __iomem *sp810_base)
 {
 	struct clk *clk;
+	const char *sp810_parent_names[] = {
+		"v2m:refclk32khz", /* REFCLK */
+		"v2m:refclk1mhz" /* TIMCLK */
+	};
+	struct clk *sp810_parent;
 	int i;
 
 	clk = clk_register_fixed_rate(NULL, "dummy_apb_pclk", NULL,
@@ -74,69 +50,38 @@ void __init vexpress_clk_init(void __iomem *sp810_base)
 			CLK_IS_ROOT, 32768);
 	WARN_ON(clk_register_clkdev(clk, NULL, "v2m:wdt"));
 
-	clk = clk_register_fixed_rate(NULL, "v2m:refclk1mhz", NULL,
+	sp810_parent = clk_register_fixed_rate(NULL, "v2m:refclk1mhz", NULL,
 			CLK_IS_ROOT, 1000000);
 
-	vexpress_sp810_init(sp810_base);
+	clk = clk_register_mux(NULL, "timerclken0",
+			sp810_parent_names, 2, 0, sp810_base + SCCTRL,
+			SCCTRL_TIMERENnSEL_SHIFT(0), 1,
+			0, &vexpress_sp810_lock);
+	WARN_ON(clk_set_parent(clk, sp810_parent));
+	WARN_ON(clk_register_clkdev(clk, "v2m-timer0", "sp804"));
 
-	for (i = 0; i < ARRAY_SIZE(vexpress_sp810_timerclken); i++)
-		WARN_ON(clk_set_parent(vexpress_sp810_timerclken[i], clk));
-
-	WARN_ON(clk_register_clkdev(vexpress_sp810_timerclken[0],
-				"v2m-timer0", "sp804"));
-	WARN_ON(clk_register_clkdev(vexpress_sp810_timerclken[1],
-				"v2m-timer1", "sp804"));
+	clk = clk_register_mux(NULL, "timerclken1",
+			sp810_parent_names, 2, 0, sp810_base + SCCTRL,
+			SCCTRL_TIMERENnSEL_SHIFT(1), 1,
+			0, &vexpress_sp810_lock);
+	WARN_ON(clk_set_parent(clk, sp810_parent));
+	WARN_ON(clk_register_clkdev(clk, "v2m-timer1", "sp804"));
 }
 
 #if defined(CONFIG_OF)
 
-struct clk *vexpress_sp810_of_get(struct of_phandle_args *clkspec, void *data)
-{
-	if (WARN_ON(clkspec->args_count != 1 || clkspec->args[0] >
-			ARRAY_SIZE(vexpress_sp810_timerclken)))
-		return NULL;
+extern void clk_sp810_of_setup(struct device_node *node);
 
-	return vexpress_sp810_timerclken[clkspec->args[0]];
-}
-
-static const __initconst struct of_device_id vexpress_fixed_clk_match[] = {
+static const __initconst struct of_device_id vexpress_clk_match[] = {
 	{ .compatible = "fixed-clock", .data = of_fixed_clk_setup, },
+	{ .compatible = "arm,sp810", .data = clk_sp810_of_setup, },
 	{ .compatible = "arm,vexpress-osc", .data = vexpress_osc_of_setup, },
 	{}
 };
 
 void __init vexpress_clk_of_init(void)
 {
-	struct device_node *node;
-	struct clk *clk;
-	struct clk *refclk, *timclk;
-
-	of_clk_init(vexpress_fixed_clk_match);
-
-	node = of_find_compatible_node(NULL, NULL, "arm,sp810");
-	vexpress_sp810_init(of_iomap(node, 0));
-	of_clk_add_provider(node, vexpress_sp810_of_get, NULL);
-
-	/* Select "better" (faster) parent for SP804 timers */
-	refclk = of_clk_get_by_name(node, "refclk");
-	timclk = of_clk_get_by_name(node, "timclk");
-	if (!WARN_ON(IS_ERR(refclk) || IS_ERR(timclk))) {
-		int i = 0;
-
-		if (clk_get_rate(refclk) > clk_get_rate(timclk))
-			clk = refclk;
-		else
-			clk = timclk;
-
-		for (i = 0; i < ARRAY_SIZE(vexpress_sp810_timerclken); i++)
-			WARN_ON(clk_set_parent(vexpress_sp810_timerclken[i],
-					clk));
-	}
-
-	WARN_ON(clk_register_clkdev(vexpress_sp810_timerclken[0],
-				"v2m-timer0", "sp804"));
-	WARN_ON(clk_register_clkdev(vexpress_sp810_timerclken[1],
-				"v2m-timer1", "sp804"));
+	of_clk_init(vexpress_clk_match);
 }
 
 #endif
