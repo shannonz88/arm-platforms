7de6529d090f0b01b6e6f3c2f56d6bfb5e67535f
Marc Zyngier <marc.zyngier@arm.com>
2014-04-15 16:02:21 +0100

    arm64: rtc: plug the PC CMOS RTC using MMIO accessors
    Add the now minimal gunk to enable the PC-style CMOS RTC.
    
    Signed-off-by: Marc Zyngier <marc.zyngier@arm.com>

---
+++ arch/arm64/include/asm/mc146818rtc.h
@@ -0,0 +1,10 @@
+/*
+ * Machine dependent access functions for RTC registers.
+ * In the arm64 case, pretty much nothing.
+ */
+#ifndef __ARM64_MC146818RTC_H__
+#define __ARM64_MC146818RTC_H__
+
+#define RTC_ALWAYS_BCD	0
+
+#endif /* __ARM64_MC146818RTC_H__ */
