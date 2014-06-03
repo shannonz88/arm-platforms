eba34bd4e3fe716b51686b145bee87edc00c1a5b
Andre Przywara <andre.przywara@arm.com>
2014-06-03 09:33:10 +0200

    arm/arm64: KVM: pass down user space provided GIC type into vGIC code
    With the introduction of a second emulated GIC model we need to let
    userspace specify the GIC model to use for each VM. Pass the
    userspace provided value down into the vGIC code to differentiate
    later.
    
    Signed-off-by: Andre Przywara <andre.przywara@arm.com>

--- virt/kvm/arm/vgic.c
+++ virt/kvm/arm/vgic.c
@@ -1992,7 +1992,7 @@ out:
 	return ret;
 }
 
-int kvm_vgic_create(struct kvm *kvm)
+int kvm_vgic_create(struct kvm *kvm, u32 type)
 {
 	int i, vcpu_lock_idx = -1, ret = 0;
 	struct kvm_vcpu *vcpu;
@@ -2456,7 +2456,7 @@ static void vgic_destroy(struct kvm_device *dev)
 
 static int vgic_create(struct kvm_device *dev, u32 type)
 {
-	return kvm_vgic_create(dev->kvm);
+	return kvm_vgic_create(dev->kvm, type);
 }
 
 struct kvm_device_ops kvm_arm_vgic_v2_ops = {
