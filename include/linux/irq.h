4628af00a62f56e9d605b245016e2b8c66fda78b
Marc Zyngier <marc.zyngier@arm.com>
2014-06-23 11:53:37 +0100

    genirq: Add IRQD_IRQ_FORWARDED flag and accessors
    Some interrupt controllers support a split EOI behaviour, where
    an active interrupt can be forwarded to a VM, and directly EOIed
    from the guest.
    
    Allow the irq_data to carry a flag describing this setup.
    
    Signed-off-by: Marc Zyngier <marc.zyngier@arm.com>

--- include/linux/irq.h
+++ include/linux/irq.h
@@ -186,6 +186,7 @@ enum {
 	IRQD_IRQ_DISABLED		= (1 << 16),
 	IRQD_IRQ_MASKED			= (1 << 17),
 	IRQD_IRQ_INPROGRESS		= (1 << 18),
+	IRQD_IRQ_FORWARDED		= (1 << 19),
 };
 
 static inline bool irqd_is_setaffinity_pending(struct irq_data *d)
@@ -270,6 +271,21 @@ static inline void irqd_set_chained_irq_inprogress(struct irq_data *d)
 static inline void irqd_clr_chained_irq_inprogress(struct irq_data *d)
 {
 	d->state_use_accessors &= ~IRQD_IRQ_INPROGRESS;
+}
+
+static inline void irqd_set_irq_forwarded(struct irq_data *d)
+{
+	d->state_use_accessors |= IRQD_IRQ_FORWARDED;
+}
+
+static inline void irqd_clr_irq_forwarded(struct irq_data *d)
+{
+	d->state_use_accessors &= ~IRQD_IRQ_FORWARDED;
+}
+
+static inline bool irqd_irq_forwarded(struct irq_data *d)
+{
+	return d->state_use_accessors & IRQD_IRQ_FORWARDED;
 }
 
 static inline irq_hw_number_t irqd_to_hwirq(struct irq_data *d)
