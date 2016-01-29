c127c04b2f897e340ffd3cbcf806170d0ddb4e19
Marc Zyngier <marc.zyngier@arm.com>
2016-01-29 15:01:28 +0000

    arm/arm64: KVM: Handle out-of-RAM cache maintenance as a NOP
    So far, our handling of cache maintenance by VA has been pretty
    simple: Either the access is in the guest RAM and generates a S2
    fault, which results in the page being mapped RW, or we go down
    the io_mem_abort() path, and nuke the guest.
    
    The first one is fine, but the second one is extremely weird.
    Treating the CM as an I/O is wrong, and nothing in the ARM ARM
    indicates that we should generate a fault for something that
    cannot end-up in the cache anyway (even if the guest maps it,
    it will keep on faulting at stage-2 for emulation).
    
    So let's just skip this instruction, and let the guest get away
    with it.
    
    Signed-off-by: Marc Zyngier <marc.zyngier@arm.com>

--- arch/arm/kvm/mmu.c
+++ arch/arm/kvm/mmu.c
@@ -1431,6 +1431,23 @@ int kvm_handle_guest_abort(struct kvm_vcpu *vcpu, struct kvm_run *run)
 		}
 
 		/*
+		 * Check for a cache maintenance operation. Since we
+		 * ended-up here, we know it is outside of any memory
+		 * slot. But we can't find out if that is for a device,
+		 * or if the guest is just being stupid (going all the
+		 * way to userspace is not an option - what would you
+		 * write?).
+		 *
+		 * So let's assume that the guest is just being
+		 * cautious, and skip the instruction.
+		 */
+		if (kvm_vcpu_dabt_iscm(vcpu)) {
+			kvm_skip_instr(vcpu, kvm_vcpu_trap_il_is32bit(vcpu));
+			ret = 1;
+			goto out_unlock;
+		}
+
+		/*
 		 * The IPA is reported as [MAX:12], so we need to
 		 * complement it with the bottom 12 bits from the
 		 * faulting VA. This is always 12 bits, irrespective
