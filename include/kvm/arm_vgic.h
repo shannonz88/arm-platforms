af2e1edb43ab76d248f8dc3c0c7c2b0cbd4740bf
Andre Przywara <andre.przywara@arm.com>
2016-03-07 17:32:29 +0700

    KVM: arm/arm64: pmu: abstract access to number of SPIs
    Currently the PMU uses a member of the struct vgic_dist directly,
    which not only breaks abstraction, but will fail with the new VGIC.
    Abstract this access in the VGIC header file.
    
    Signed-off-by: Andre Przywara <andre.przywara@arm.com>

--- include/kvm/arm_vgic.h
+++ include/kvm/arm_vgic.h
@@ -347,6 +347,8 @@ bool kvm_vgic_map_is_active(struct kvm_vcpu *vcpu, unsigned int virt_irq);
 #define irqchip_in_kernel(k)	(!!((k)->arch.vgic.in_kernel))
 #define vgic_initialized(k)	(!!((k)->arch.vgic.nr_cpus))
 #define vgic_ready(k)		((k)->arch.vgic.ready)
+#define vgic_valid_spi(k,i)	(((i) >= VGIC_NR_PRIVATE_IRQS) && \
+				 ((i) < (k)->arch.vgic.nr_irqs))
 
 int vgic_v2_probe(struct device_node *vgic_node,
 		  const struct vgic_ops **ops,
